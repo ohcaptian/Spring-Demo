85f4afffc6b3be62162322ff7defc4ed879128ac
cz_captain <cz_captain@163.com>
2020-06-08 19:07:08 +0800

    实现了删除功能

--- employee/src/main/java/com/ying/Controller/EmployeeController.java
+++ employee/src/main/java/com/ying/Controller/EmployeeController.java
@@ -1,14 +1,16 @@
 package com.ying.Controller;
 
 
+import com.ying.dao.DepartmentDao;
 import com.ying.dao.EmployeeDao;
+import com.ying.pojo.Department;
 import com.ying.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.jws.WebParam;
 import java.util.Collection;
 
 @Controller
@@ -17,6 +19,8 @@ public class EmployeeController {
 
     @Autowired
     public EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
 
     @RequestMapping("/emps")
     public String list(Model model){
@@ -25,4 +29,27 @@ public class EmployeeController {
             return "list";
     }
 
+    @GetMapping("/emp")
+    public String addEmp(Model model){
+       Collection<Department> departments= departmentDao.getDepartments();
+       model.addAttribute("dept",departments);
+        return "add";
+    }
+    @PostMapping("/emp")
+    public String add(Employee employee){
+        employeeDao.addEmployee(employee);
+        return "redirect:/emps";
+    }
+    //等待完成
+    /*@GetMapping("/emp/{id}")
+    public String updata(@PathVariable Integer id){
+
+
+            return "update";
+    }  */
+    @GetMapping("/delemp/{id}")
+    public String delete(@PathVariable("id") Integer id){
+        employeeDao.delectEmployeeById(id);
+        return "redirect:/emps";
+    }
 }
