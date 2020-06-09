203cf9248a5ed0fb5b22ab497d04cf2ee2803978
cz_captain <cz_captain@163.com>
2020-06-09 15:19:19 +0800

    完成增删改查

--- employee/src/main/java/com/ying/Controller/EmployeeController.java
+++ employee/src/main/java/com/ying/Controller/EmployeeController.java
@@ -35,18 +35,34 @@ public class EmployeeController {
        model.addAttribute("dept",departments);
         return "add";
     }
+
+    //页面的复用
     @PostMapping("/emp")
     public String add(Employee employee){
         employeeDao.addEmployee(employee);
         return "redirect:/emps";
     }
-    //等待完成
-    /*@GetMapping("/emp/{id}")
-    public String updata(@PathVariable Integer id){
-
-
-            return "update";
-    }  */
+    //等待完成、
+    /*
+              Get Post等restful风格
+     */
+    //去员工的修改页面
+    @GetMapping("/emp/{id}")
+    public String updata(@PathVariable("id") Integer id, Model model){
+       Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("emp",employee);
+        //查询部门信息
+        Collection<Department> departments= departmentDao.getDepartments();
+        model.addAttribute("dept",departments);
+        return "update";
+    }
+    //员工信息修改
+    @PostMapping("/updateemp")
+    public String update(Employee employee){
+        employeeDao.addEmployee(employee);
+        return "redirect:/emps";
+    }
+        //删除员工
     @GetMapping("/delemp/{id}")
     public String delete(@PathVariable("id") Integer id){
         employeeDao.delectEmployeeById(id);
