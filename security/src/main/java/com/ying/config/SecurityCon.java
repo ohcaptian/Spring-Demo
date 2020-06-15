fa8fd2514b073b61c2b8ffb890bd9bdc17dbd945
cz_captain <cz_captain@163.com>
2020-06-15 18:39:14 +0800

    尝试security

---
+++ security/src/main/java/com/ying/config/SecurityCon.java
@@ -0,0 +1,55 @@
+package com.ying.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class SecurityCon{
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+//        return manager;
+//    }
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        // ensure the passwords are encoded properly
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("password").roles("USER").build());
+        manager.createUser(users.username("admin").password("password").roles("USER","ADMIN").build());
+        return manager;
+    }
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/level3/**")
+                    .authorizeRequests(authorize -> authorize
+                            .anyRequest().hasRole("ADMIN")
+                    )
+                    .httpBasic();
+        }
+    }
+    @Configuration
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests(authorize -> authorize
+                            .anyRequest().authenticated()
+                    )
+                    .formLogin();
+        }
+    }
+}
